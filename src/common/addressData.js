7cd1653a45879760a337ad8a8005743ed57550ed
zengjiachengcode <2356924146@qq.com>
2021-10-26 10:39:31 +0800

    address

--- src/common/addressData.js
+++ src/common/addressData.js
@@ -3,10 +3,10 @@ export default [
     text: '实训楼',
     value: '330000',
     children: [
-      { text: '实训楼1栋', value: '1' },
-      { text: '实训楼2栋', value: '2' },
-      { text: '实训楼3栋', value: '3' },
-      { text: '实训楼4栋', value: '4' }
+      { text: '1栋', value: '1' },
+      { text: '2栋', value: '2' },
+      { text: '3栋', value: '3' },
+      { text: '4栋', value: '4' }
     ]
   },
   {
